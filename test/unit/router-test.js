c804b58dab01c4d3fb4147cf731298bce1d8c28e
David Baker <dbaker@acorncomputersolutions.com>
2016-10-12 15:57:00 -0600

    fix(data): fix serializer problems hit when serializer = false
    In an Action, `serializer = false` is something we're allowed to do.
    However, if we're doing a POST of data into Denali (using a webhook)
    and have a resulting request.body, we weren't checking whether a
    serializer was desired, but were instead using the application
    serializer on the body.
    
    In my case, I wanted the raw POST body to make it through untouched.
    This fixes that ...

---
+++ test/unit/router-test.js
@@ -0,0 +1,89 @@
+import expect from 'must';
+import Action from '../../lib/runtime/action';
+import Router from '../../lib/runtime/router';
+import Container from '../../lib/runtime/container';
+import FlatSerializer from '../../lib/runtime/base/app/serializers/flat';
+import JSONAPISerializer from '../../lib/runtime/base/app/serializers/json-api';
+import merge from 'lodash/merge';
+
+function mockReqRes(overrides) {
+  let container = new Container();
+
+  container.register('serializer:application', FlatSerializer);
+  container.register('config:environment', {});
+
+  return merge({
+    container,
+    request: {
+      get(headerName) {
+        return this.headers && this.headers[headerName.toLowerCase()];
+      },
+      headers: {
+        'content-type': 'application/json'
+      },
+      query: {},
+      body: {}
+    },
+    response: {
+      write() {},
+      setHeader() {},
+      render() {},
+      end() {}
+    },
+    next() {}
+  }, overrides);
+}
+
+describe.only('Denali.Router', function() {
+
+  it('should not invoke the default serializer if serializer=false in an action', function() {
+    let success = false;
+    let mock = mockReqRes({
+      request: {
+        headers: {
+          'content-type': 'application/json',
+          'transfer-encoding': 'chunked'
+        },
+        method: 'post',
+        url: '/a-path',
+        body: { my: 'post-data' }
+      }
+    });
+    class TestAction extends Action {
+      serializer = false;
+      run() {
+        return new Promise((resolve) => {
+          success = true;
+          resolve();
+        });
+      }
+    }
+
+    // if we do run through this serializer, it will blow up with errors
+    // about the data key which we don't have'
+    mock.container.register('serializer:application', {
+      parse() {
+        throw new Error('Must not use the serializer');
+      }
+    });
+    class TestRouter extends Router {
+      routes = {
+        post: [ {
+          action: TestAction,
+          match() {
+            return true;
+          }
+        } ]
+      }
+      handleError(req, res, uncaughtError) {
+        expect(uncaughtError).to.be.null();
+      }
+    }
+    let router = new TestRouter(mock);
+
+    router.handle(mock.request, mock.response).then(() => {
+      expect(success).to.be.true();
+    });
+  });
+
+});
