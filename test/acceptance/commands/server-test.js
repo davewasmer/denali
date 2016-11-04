42f3e0f966e4003dcf9ecc48142c63fc5d5b25fb
Dave Wasmer <davewasmer@gmail.com>
2016-11-03 22:22:14 -0600

    convert from recursive build of addons to flattened build, mimicing yarn/npm3

--- test/acceptance/commands/server-test.js
+++ test/acceptance/commands/server-test.js
@@ -26,18 +26,13 @@ test('server command > launches a server', async () => {
 });
 
 test('server command > launches a server based on the dummy app in an addon', async () => {
-  console.log('starting test');
   let generateAddon = new CommandAcceptanceTest('addon foobar --use-npm', { populateWithDummy: false });
-  console.log('generating addon');
   await generateAddon.run();
-  console.log('addon generated');
   linkDependency(path.join(generateAddon.dir, 'foobar'), 'denali', generateAddon.projectRoot);
   let server = new CommandAcceptanceTest('server --port 3002', {
     dir: path.join(generateAddon.dir, 'foobar'),
     populateWithDummy: false
   });
-  console.log(generateAddon.dir);
-  await new Promise((resolve) => {});
 
   return server.spawn({
     env: {
